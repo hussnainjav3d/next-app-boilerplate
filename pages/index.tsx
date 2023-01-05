3c09b8a234073da0936e8196548475edb6eb144e
Hussnain Javed <hussnain@bracketsltd.email>
2023-01-05 13:04:23 +0500

    build: add debugging configuration

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,10 @@
+import { Inter } from '@next/font/google'
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import styles from '../styles/Home.module.css'
 
-const inter = Inter({ subsets: [`latin`] })
+// eslint-disable-next-line quotes
+const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
